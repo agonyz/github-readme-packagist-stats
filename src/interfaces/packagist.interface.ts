2f1e791befd68d91b5c434044a4f747d22f561fb
agonyz <agonyz@outlook.de>
2023-07-16 19:00:44 +0200

    improve packagist service

--- src/interfaces/packagist.interface.ts
+++
@@ -1,6 +0,0 @@
-import { PackageDownloadStats } from '@agonyz/packagist-api-client/lib/interfaces';
-
-export interface SortedPackage {
-  packageName: string;
-  downloadStats: PackageDownloadStats;
-}
